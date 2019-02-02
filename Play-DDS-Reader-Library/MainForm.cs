f81d069c2c46f0e0c22bf92b2e26fac6dc804f09
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-02-02 23:56:06 +0700

    Initial files

---
+++ Play-DDS-Reader-Library/MainForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using GDImageLibrary;
+using System.Drawing.Imaging;
+
+namespace Play_DDS_Reader_Library
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = @"C:\Users\Yummy PC\Downloads\ygLight_4_0_6\Image\";
+            //string[] dirs = { "ITEM", "MAP", "MUGONG", "SKILL" };
+            //foreach (string dir in dirs)
+            //{
+            //MessageBox.Show(path + dir);
+            //Convert(path, dir);
+            //}
+            Convert(path, @"ITEM\Random");
+        }
+
+        static void Convert(string path, string directoryName)
+        {
+            string outputPath = Path.Combine(Environment.CurrentDirectory, directoryName + "_PNG");
+            if (!Directory.Exists(outputPath))
+            {
+                Directory.CreateDirectory(outputPath);
+            }
+            DirectoryInfo directoryInfo = new DirectoryInfo(Path.Combine(path, directoryName));
+            foreach (var fileInfo in directoryInfo.GetFiles("*.d2s"))
+            {
+                try
+                {
+                    Bitmap bitmap = _DDS.LoadImage(fileInfo.FullName);
+                    string resultPath = Path.Combine(outputPath, fileInfo.Name.ToLower().Replace(".d2s", ".png"));
+                    bitmap.Save(resultPath, ImageFormat.Png);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
